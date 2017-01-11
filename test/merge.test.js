5a23cf7c84fbe99d29a8cca818bbe701e6369a77
GuoKai <badkaikai@gmail.com>
2017-01-11 19:02:57 +0800

    update for test.

---
+++ test/merge.test.js
@@ -0,0 +1,9 @@
+/* global describe, it */
+
+describe('DecryptTheClientSideToken', function () {
+    describe('#decryptResult()', function () {
+        it('should return correct `decryptedTokenInfo` when the token is exist', function (done) {
+            done();
+        });
+    });
+});
