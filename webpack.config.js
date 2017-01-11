32e0145b756c83bfed6f61cfd19f1edcd7178604
GuoKai <badkaikai@gmail.com>
2017-01-11 16:10:14 +0800

    webpack build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: './inc.js',
     output: {
         filename: 'inc.js',
         path: './dist'
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
 };
