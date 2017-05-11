76826812c50eda8d61007ecf8596fc85d188a126
yangxiaojie03 <yangxiaojie03@meituan.com>
2017-05-11 16:46:16 +0800

    fix index

--- dist/inc.js
+++ dist/inc.js
@@ -64,12 +64,14 @@
 
 	var _config2 = _interopRequireDefault(_config);
 
+	var _store = __webpack_require__(4);
+
+	var _store2 = _interopRequireDefault(_store);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } } /*eslint no-eval: 0, no-useless-call: 0*/
 
-
-	// import Merge from './merge';
 
 	var makeArray = function makeArray(names) {
 	    var arr = [];
@@ -83,6 +85,7 @@
 	    function Inc() {
 	        _classCallCheck(this, Inc);
 
+	        this.activated = true; // 是否开启前端离线化
 	        this.queue = {}; // 待执行模块
 
 	        var scripts = makeArray(document.getElementsByTagName('script'));
@@ -152,6 +155,39 @@
 	    };
 
 	    /**
+	     * 加载模块内容
+	     * @param {String} name
+	     * @param {Function} callback
+	     */
+
+
+	    Inc.prototype._runLoaded = function _runLoaded(name, callback) {
+	        var module = this.queue[name];
+	        if (module.content) {
+	            // 模块内容存在则直接执行 callback
+	            callback(module);
+	            return;
+	        }
+
+	        var store = new _store2.default(this.config, this.hook);
+	        if (!store.isSupported()) {
+	            // 存储不可用则直接执行 callback
+	            callback(module);
+	            return;
+	        }
+
+	        var data = store.getItem(module.name);
+	        var url = data.u,
+	            version = data.v,
+	            content = data.c;
+
+	        console.log(url, version, content);
+	        if (version === module.version) {
+	            console.log(version);
+	        }
+	    };
+
+	    /**
 	     * 执行加载队列
 	     * @param {Array} args
 	     * @param {Function} callback
@@ -160,6 +196,12 @@
 
 	    Inc.prototype._runQueue = function _runQueue(args, callback) {
 	        var _this = this;
+
+	        if (!this.activated) {
+	            // 不使用前端离线化则直接执行 callback
+	            callback();
+	            return;
+	        }
 
 	        var modules = [];
 
@@ -178,6 +220,12 @@
 	        };
 
 	        tree(args);
+
+	        for (var i = 0; i < modules.length; i++) {
+	            this._runLoaded(modules[i], function () {
+	                console.log(12);
+	            });
+	        }
 	        console.log(modules);
 	        console.log(callback);
 	    };
@@ -341,6 +389,106 @@
 	}();
 
 	exports.default = Config;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	exports.__esModule = true;
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Store = function () {
+	    function Store(config, hook) {
+	        _classCallCheck(this, Store);
+
+	        this.hook = hook;
+	        this.prefix = config.get('bizname'); // 储存前缀
+	        this.maxSize = config.get('storeMaxSize'); // 存储最大大小
+
+	        this.currentSize = -1; // 存储当前大小
+	        if (this.isSupported()) {
+	            this.currentSize = localStorage.getItem(this.prefix + ':incCurrentSize') || -1;
+	            this.hook.push('storeCurrentSize', this.currentSize);
+	        }
+	    }
+
+	    /**
+	     * 判断当前环境是否支持 Localstorage
+	     * @return {Boolean}
+	     */
+
+
+	    Store.prototype.isSupported = function isSupported() {
+	        try {
+	            // api 支持情况
+	            if (!('localStorage' in window && window.localStorage)) {
+	                return false;
+	            }
+	            /**
+	             * 存储异常情况下
+	             * 1.已经存满
+	             * 2.只读情况(隐身模式)
+	             */
+	            localStorage.setItem('-.-', 1);
+	            localStorage.removeItem('-.-');
+	        } catch (err) {
+	            localStorage.clear();
+	            return false;
+	        }
+	        return true;
+	    };
+
+	    /**
+	     * 清楚超出 maxSize 大小的最老数据
+	     * @return {Boolean}
+	     */
+
+
+	    Store.prototype.clearSuperfluous = function clearSuperfluous() {}
+	    // TODO
+
+
+	    /**
+	     * 获取 Localstorage 项
+	     * @param {string} key
+	     * @return {Object} { 'u': url地址, 'v': 版本号, 'c': 实际内容, 't': 时间戳, 's': 内容大小 }
+	     */
+	    ;
+
+	    Store.prototype.getItem = function getItem(key) {}
+	    // TODO
+
+
+	    /**
+	     * 保存 Localstorage 项
+	     * @param {string} key
+	     * @param {Object} { 'u': url地址, 'v': 版本号, 'c': 实际内容, 't': 时间戳, 's': 内容大小 }
+	     * @return {Boolean}
+	     */
+	    ;
+
+	    Store.prototype.setItem = function setItem(key, value) {}
+	    // TODO
+
+
+	    /**
+	     * 删除 Localstorage 项
+	     * @param {string} key
+	     * @return {Boolean}
+	     */
+	    ;
+
+	    Store.prototype.removeItem = function removeItem(key) {
+	        // TODO
+	    };
+
+	    return Store;
+	}();
+
+	exports.default = Store;
 
 /***/ }
 /******/ ]);
